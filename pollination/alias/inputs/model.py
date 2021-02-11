edb09dcec9fdc7f988dd0f74a925623593674c2f
Chris Mackey <chris@ladybug.tools>
2021-02-11 12:55:52 -0500

    feat(energy): Add aliases for all energy-related simulation recipes
    I am also updating the daylight output aliases based on changes in the handlers.

--- pollination/alias/inputs/model.py
+++ pollination/alias/inputs/model.py
@@ -2,13 +2,13 @@ from pollination_dsl.alias import InputAlias
 from queenbee.io.common import IOAliasHandler
 
 
-"""Alias input for inputs that expect a HBJSON model file as the inputs."""
+"""Alias inputs that expect a HBJSON model file as the recipe input."""
 hbjson_model_input = [
     # grasshopper Alias
     InputAlias.any(
         name='model',
         description='A path to a HBJSON file or a HB model object built with Python or'
-        'dotnet libraries.',
+        ' dotnet libraries.',
         platform=['grasshopper'],
         handler=[
             IOAliasHandler(
@@ -30,6 +30,24 @@ hbjson_model_input = [
             IOAliasHandler(
                 language='csharp', module='HoneybeeRhino.Handlers',
                 function='RhinoHBModelToJSON'
+            )
+        ]
+    )
+]
+
+
+"""Alias inputs that expect a DFJSON model file as the recipe input."""
+dfjson_model_input = [
+    # grasshopper Alias
+    InputAlias.any(
+        name='model',
+        description='A path to a DFJSON file or a DF model object built with Python '
+        ' libraries.',
+        platform=['grasshopper'],
+        handler=[
+            IOAliasHandler(
+                language='python', module='pollination_handlers.inputs.model',
+                function='model_dragonfly_to_json'
             )
         ]
     )
--- pollination/alias/outputs/daylight.py
+++ pollination/alias/outputs/daylight.py
@@ -14,8 +14,28 @@ parse_daylight_factor_results = [
         handler=[
             IOAliasHandler(
                 language='python',
-                module='pollination_handlers.outputs.read_file',
-                function='read_DF_from_path'
+                module='pollination_handlers.outputs.daylight',
+                function='read_df_from_folder'
+            )
+        ]
+    )
+]
+
+
+"""Annual daylight recipe output.
+
+Sort the .ill result files output by an annual daylight simulation.
+"""
+sort_annual_daylight_results = [
+    OutputAlias.any(
+        name='annual_daylight',
+        description='Annual daylight result files.',
+        platform=['grasshopper'],
+        handler=[
+            IOAliasHandler(
+                language='python',
+                module='pollination_handlers.outputs.daylight',
+                function='sort_ill_from_folder'
             )
         ]
     )
