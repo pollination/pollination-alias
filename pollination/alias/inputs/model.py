e88dde1978d1f2f42214b812febdc31fdad3c445
Antonello Di Nunzio <plugin@antonellodinunzio.online>
2023-05-23 17:01:52 +0200

    fix(model): use python with revit

--- pollination/alias/inputs/model.py
+++ pollination/alias/inputs/model.py
@@ -4,13 +4,13 @@ from queenbee.io.common import IOAliasHandler
 
 """Alias inputs that expect a HBJSON model file as the recipe input."""
 hbjson_model_input = [
-    # grasshopper Alias
+    # RH GH Alias
     InputAlias.any(
         name='model',
         description='A Honeybee Model to simulate or the path to a HBJSON file '
         'of a Model. This can also be the path to a HBpkl file, though this is only '
         'recommended for cases where the model is extremely large.',
-        platform=['grasshopper', 'rhino', 'revit'],
+        platform=['grasshopper', 'rhino'],
         handler=[
             IOAliasHandler(
                 language='python', module='pollination_handlers.inputs.model',
@@ -21,20 +21,33 @@ hbjson_model_input = [
                 function='HBModelToJSON'
             )
         ]
+    ),
+    InputAlias.any(
+        name='model',
+        description='A Honeybee Model to simulate or the path to a HBJSON file '
+        'of a Model. This can also be the path to a HBpkl file, though this is only '
+        'recommended for cases where the model is extremely large.',
+        platform=['revit'],
+        handler=[
+            IOAliasHandler(
+                language='python', module='pollination_handlers.inputs.model',
+                function='model_to_json'
+            )
+        ]
     )
 ]
 
 
 """Alias inputs that expect a HBJSON model with sensor grids."""
 hbjson_model_grid_input = [
-    # grasshopper Alias
+    # RH GH Alias
     InputAlias.any(
         name='model',
         description='A Honeybee Model to simulate or the path to a HBJSON file '
         'of a Model. This can also be the path to a HBpkl file, though this is only '
         'recommended for cases where the model is extremely large. Note that this '
         'model should have sensor grids assigned to it.',
-        platform=['grasshopper', 'rhino', 'revit'],
+        platform=['grasshopper', 'rhino'],
         handler=[
             IOAliasHandler(
                 language='python', module='pollination_handlers.inputs.model',
@@ -45,19 +58,33 @@ hbjson_model_grid_input = [
                 function='HBModelToJSON'
             )
         ]
+    ),
+    InputAlias.any(
+        name='model',
+        description='A Honeybee Model to simulate or the path to a HBJSON file '
+        'of a Model. This can also be the path to a HBpkl file, though this is only '
+        'recommended for cases where the model is extremely large. Note that this '
+        'model should have sensor grids assigned to it.',
+        platform=['revit'],
+        handler=[
+            IOAliasHandler(
+                language='python', module='pollination_handlers.inputs.model',
+                function='model_to_json_grid_check'
+            )
+        ]
     )
 ]
 
 
 """Alias inputs that expect a HBJSON model with sensor grids and rooms."""
 hbjson_model_grid_room_input = [
-    # grasshopper Alias
+    # RH GH Alias
     InputAlias.any(
         name='model',
         description='A Honeybee Model to simulate or the path to a HBJSON file '
         'of a Model. Note that this model must contain rooms and have sensor '
         'grids assigned to it.',
-        platform=['grasshopper', 'rhino', 'revit'],
+        platform=['grasshopper', 'rhino'],
         handler=[
             IOAliasHandler(
                 language='python', module='pollination_handlers.inputs.model',
@@ -68,20 +95,33 @@ hbjson_model_grid_room_input = [
                 function='HBModelToJSON'
             )
         ]
+    ),
+    InputAlias.any(
+        name='model',
+        description='A Honeybee Model to simulate or the path to a HBJSON file '
+        'of a Model. Note that this model must contain rooms and have sensor '
+        'grids assigned to it.',
+        platform=['revit'],
+        handler=[
+            IOAliasHandler(
+                language='python', module='pollination_handlers.inputs.model',
+                function='model_to_json_grid_room_check'
+            )
+        ]
     )
 ]
 
 
 """Alias inputs that expect a HBJSON model with Rooms."""
 hbjson_model_room_input = [
-    # grasshopper Alias
+    # RH GH Alias
     InputAlias.any(
         name='model',
         description='A Honeybee Model to simulate or the path to a HBJSON file '
         'of a Model. This can also be the path to a HBpkl file, though this is only '
         'recommended for cases where the model is extremely large. Note that this '
         'model should have Rooms assigned to it to be usable with this recipe.',
-        platform=['grasshopper', 'rhino', 'revit'],
+        platform=['grasshopper', 'rhino'],
         handler=[
             IOAliasHandler(
                 language='python', module='pollination_handlers.inputs.model',
@@ -92,13 +132,27 @@ hbjson_model_room_input = [
                 function='HBModelToJSON'
             )
         ]
+    ),
+    InputAlias.any(
+        name='model',
+        description='A Honeybee Model to simulate or the path to a HBJSON file '
+        'of a Model. This can also be the path to a HBpkl file, though this is only '
+        'recommended for cases where the model is extremely large. Note that this '
+        'model should have Rooms assigned to it to be usable with this recipe.',
+        platform=['revit'],
+        handler=[
+            IOAliasHandler(
+                language='python', module='pollination_handlers.inputs.model',
+                function='model_to_json_room_check'
+            )
+        ]
     )
 ]
 
 
 """Alias inputs that expect a HBJSON model with HVAC systems."""
 hbjson_model_hvac_input = [
-    # grasshopper Alias
+    # RH GH Alias
     InputAlias.any(
         name='model',
         description='A Honeybee Model to simulate or the path to a HBJSON file '
@@ -106,7 +160,7 @@ hbjson_model_hvac_input = [
         'have Programs, ConstructionSets, and real HVAC systems (other than Ideal Air) '
         'assigned to it. If the building has hot water loads, the building should also '
         'have a SHW system assigned to it for the results to be meaningful.',
-        platform=['grasshopper', 'rhino', 'revit'],
+        platform=['grasshopper', 'rhino'],
         handler=[
             IOAliasHandler(
                 language='python', module='pollination_handlers.inputs.model',
@@ -117,20 +171,35 @@ hbjson_model_hvac_input = [
                 function='HBModelToJSON'
             )
         ]
+    ),
+    InputAlias.any(
+        name='model',
+        description='A Honeybee Model to simulate or the path to a HBJSON file '
+        'of a Model. Note that this model should represent a full building and '
+        'have Programs, ConstructionSets, and real HVAC systems (other than Ideal Air) '
+        'assigned to it. If the building has hot water loads, the building should also '
+        'have a SHW system assigned to it for the results to be meaningful.',
+        platform=['revit'],
+        handler=[
+            IOAliasHandler(
+                language='python', module='pollination_handlers.inputs.model',
+                function='model_to_json_hvac_check'
+            )
+        ]
     )
 ]
 
 
 """Alias inputs that expect a HBJSON model with views."""
 hbjson_model_view_input = [
-    # grasshopper Alias
+    # RH GH Alias
     InputAlias.any(
         name='model',
         description='A Honeybee Model to simulate or the path to a HBJSON file '
         'of a Model. This can also be the path to a HBpkl file, though this is only '
         'recommended for cases where the model is extremely large. Note that this '
         'model should have views assigned to it.',
-        platform=['grasshopper', 'rhino', 'revit'],
+        platform=['grasshopper', 'rhino'],
         handler=[
             IOAliasHandler(
                 language='python', module='pollination_handlers.inputs.model',
@@ -139,6 +208,20 @@ hbjson_model_view_input = [
             IOAliasHandler(
                 language='csharp', module='Pollination.RhinoHandlers',
                 function='HBModelToJSON'
+            )
+        ]
+    ),
+    InputAlias.any(
+        name='model',
+        description='A Honeybee Model to simulate or the path to a HBJSON file '
+        'of a Model. This can also be the path to a HBpkl file, though this is only '
+        'recommended for cases where the model is extremely large. Note that this '
+        'model should have views assigned to it.',
+        platform=['revit'],
+        handler=[
+            IOAliasHandler(
+                language='python', module='pollination_handlers.inputs.model',
+                function='model_to_json_view_check'
             )
         ]
     )
