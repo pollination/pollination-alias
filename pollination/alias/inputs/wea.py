69f55d8066a6e9b59631cd5add6f675e171faf15
Mikkel Pedersen <mkklpdrsn@gmail.com>
2025-02-27 01:17:16 +0100

    fix(wea): Add wea alias for timestep and annual data

--- pollination/alias/inputs/wea.py
+++ pollination/alias/inputs/wea.py
@@ -49,3 +49,21 @@ wea_input_timestep_check = [
         ]
     )
 ]
+
+
+"""Alias for inputs that expect a .wea file at a timestep of 1 and annual data."""
+wea_input_timestep_annual_check = [
+    InputAlias.any(
+        name='wea',
+        description='A Wea object produced from the Wea components that are under '
+        'the Light Sources tab. This can also be the path to a .wea or a .epw file.',
+        platform=['grasshopper', 'rhino', 'revit'],
+        handler=[
+            IOAliasHandler(
+                language='python',
+                module='pollination_handlers.inputs.wea',
+                function='wea_handler_timestep_annual_check'
+            )
+        ]
+    )
+]
